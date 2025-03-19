600c3a533d1bcc23fd8ccc8ad0ed142bf7bfcc3e
Enkhjargal-hub <Joloenja@gmail.com>
2025-03-19 12:05:26 +0800

    j

--- backend/server/src/middleware/authorize.js
+++ backend/server/src/middleware/authorize.js
@@ -1,24 +1,25 @@
 import jwt from "jsonwebtoken";
-import User from "../models/user-schema.js"; 
+import { UserModel } from "../models/user-schema.js"; 
 import dotenv from "dotenv";
 
 dotenv.config(); 
 
+const TOKEN_SECRET = process.env.TOKEN_SECRET; 
 
 export const authorize = async (req, res, next) => {
   try {
-    const token = req.headers.authorization?.split(" ")[1]; // Token shuud avna
+    const token = req.headers.authorization?.split(" ")[1]; 
 
-    if (!token) return res.status(401).json({ message: "Unauthorized" }); 
+    if (!token) return res.status(401).json({ message: "Unauthorized" });
 
-    const decoded = jwt.verify(token, TOKEN_SECRET);
-    req.user = await User.findById(decoded.userId);
+    const decoded = jwt.verify(token, TOKEN_SECRET); 
+    req.user = await UserModel.findById(decoded.userId); 
 
-    if (!req.user) return res.status(401).json({ message: "Unauthorized" }); 
+    if (!req.user) return res.status(401).json({ message: "Unauthorized" });
 
-    next();
+    next(); 
   } catch (err) {
-    res.status(500).json({ 
+    res.status(500).json({
       message: "Server error",
       error: err.message
     });
