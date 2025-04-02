240c4af147f092acddbb764caa04ac29beb56e19
Enkhjargal-hub <Joloenja@gmail.com>
2025-04-02 16:35:47 +0800

    j

--- frontend/client/home/page.tsx
+++ frontend/client/home/page.tsx
@@ -1,14 +1,29 @@
 "use client";
 
-import Layout from "./_components/Layout";
-import Hero from "./_components/Hero";
-import Menu from "./_components/Menu";
+import Navbar from "./_components/Navbar";
+import Order from "./_components/Order";
+import { Categories } from "./_components/Categories";
 
-export default function Home() {
+const Homepage = () => {
   return (
-    <Layout>
-      <Hero />
-      <Menu />
-    </Layout>
+    <div className="flex flex-row h-screen">
+      <Navbar />
+
+      <div
+        style={{ backgroundColor: "#0F0F0F" }}
+        className="w-full flex flex-col overflow-y-auto"
+      >
+        <div className="sticky top-0 z-10">
+          <Categories />
+        </div>
+        <div className="flex-grow overflow-y-auto mt-4"></div>
+      </div>
+
+      <div className="flex-shrink-0 w-[500px]">
+        <Order />
+      </div>
+    </div>
   );
-}
+};
+
+export default Homepage;
